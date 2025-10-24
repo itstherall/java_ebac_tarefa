07e8bdbedc89d2fcc1ef84e9e9ecf74f6cdc68a6
itstherall <14thereza@gmail.com>
2025-10-24 13:48:28 -0300

    tarefa de streams, filtro de lista para selecionar o genero feminino

---
+++ 15 Tarefas Streams/Streams/src/tarefa_streams/Pessoa.java
@@ -0,0 +1,41 @@
+package tarefa_streams;
+
+public class Pessoa {
+	
+	public String nome;
+	public String genero;
+	
+	/* ** CONSTRUTORES ** */
+	
+	public Pessoa(String nome, String genero) {
+		super();
+		this.nome = nome;
+		this.genero = genero;
+	}
+	
+	/* ** GETTERS E SETTERS ** */
+	
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	
+	/* ** FUCTIONS ** */
+	
+	
+	/* ** SOBREESCREVENDO ** */
+	public String toString() {
+        return  nome +  '|' + genero + ' ';
+    }
+
+
+}
